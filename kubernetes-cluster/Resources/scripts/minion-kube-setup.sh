f72974716f04f06a51c3c12e2d6de851e59a3d8d
drngsl <2289568746@qq.com>
2016-03-04 20:29:30 +0800

    1 support cloud config from frontend 2 if current env can't connect to cascading, then don't support cloud config

--- kubernetes-cluster/Resources/scripts/minion-kube-setup.sh
+++ kubernetes-cluster/Resources/scripts/minion-kube-setup.sh
@@ -4,19 +4,50 @@
 # $2 - IP
 # $3 - MASTER_IP
 # $4 - cAdvisor port
+# $5 - API Address
+# $6 - Auth Host
+# $7 - Auth URL
+# $8 - username/password
+# $9 - region/tenant name
 
 mkdir /var/log/kubernetes
 mkdir -p /var/run/murano-kubernetes
 
-cp cloud_conf/cloud.conf /root/
-echo '58.60.106.60   identity.cloud.hybrid.huawei.com' >> /etc/hosts
-echo '58.60.106.60   compute.cloud.hybrid.huawei.com' >> /etc/hosts
-echo '58.60.106.60   volume.cloud.hybrid.huawei.com' >> /etc/hosts
+AUTHHOST=$6
+COMPUTEHOST=${AUTHHOST/identity/compute}
+VOLUMEHOST=${AUTHHOST/identity/volume}
+
+echo "$5   ${AUTHHOST}" >> /etc/hosts
+echo "$5   ${COMPUTEHOST}" >> /etc/hosts
+echo "$5   ${VOLUMEHOST}" >> /etc/hosts
 
 SERVERID=`hostname | cut -c7-42`
 echo $SERVERID > /etc/hostname
 hostname $SERVERID
-#source /etc/hostname
+
+AUTHURI=$7
+USERNAME_PWD=$8
+REGION_TENANT=$9
+
+USERNAME=`echo $USERNAME_PWD | cut -d / -f 1`
+PASSWORD=`echo $USERNAME_PWD | cut -d / -f 2`
+REGION=`echo $REGION_TENANT | cut -d / -f 1`
+TENANT=`echo $REGION_TENANT | cut -d / -f 2`
+
+sed -i.bkp "s%AUTHURL%$7%g" cloud_conf/cloud.conf
+sed -i.bkp "s%USERNAME%${USERNAME}%g" cloud_conf/cloud.conf
+sed -i.bkp "s%PASSWORD%${PASSWORD}%g" cloud_conf/cloud.conf
+sed -i.bkp "s%REGION%${REGION}%g" cloud_conf/cloud.conf
+sed -i.bkp "s%TENANTNAME%${TENANT}%g" cloud_conf/cloud.conf
+cp cloud_conf/cloud.conf /root/
+
+BODY="{\"auth\": {\"tenantName\": \"${TENANT}\",\"passwordCredentials\": {\"username\": \"${USERNAME}\",\"password\": \"${PASSWORD}\"}}}"
+curl ${AUTHURI}/tokens -k -H "Content-Type:application/json" -X POST -d "$BODY" 2>/dev/null | grep "endpoints" >/dev/null && {
+true
+} || {
+sed -i.bkp "s%openstack% %g" default_scripts/kubelet
+sed -i.bkp "s%/root/cloud.conf% %g" default_scripts/kubelet
+}
 
 sed -i.bkp "s/%%MASTER_IP%%/$3/g" default_scripts/kube-proxy
 sed -i.bkp "s/%%MASTER_IP%%/$3/g" default_scripts/kubelet
