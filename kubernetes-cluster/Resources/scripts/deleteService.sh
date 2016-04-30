0551f1dbfe00d1502dcdb38bbc7c4079102a68f4
vklonghml <85885453@qq.com>
2016-04-30 18:18:04 +0800

    correct error: duplicated remove

--- kubernetes-cluster/Resources/scripts/deleteService.sh
+++ kubernetes-cluster/Resources/scripts/deleteService.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Deleting a Service" >> /tmp/murano-kube.log
-/opt/bin/kubectl delete service $1 >> /tmp/murano-kube.log
+#/opt/bin/kubectl delete service $1 >> /tmp/murano-kube.log
+/opt/bin/kubectl get service | awk -v name=$1 '$1==name{system("/opt/bin/kubectl delete service " name)}' >> /tmp/murano-kube.log
